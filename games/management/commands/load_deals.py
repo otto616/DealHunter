7c422de6709ee9e0ec2ab37c2d1bd98a77314822
otto616 <ottobielrodenas@gmail.com>
2026-02-17 19:41:17 +0100

    load script

--- games/management/commands/load_deals.py
+++ games/management/commands/load_deals.py
@@ -21,26 +21,31 @@ class Command(BaseCommand):
             Shop.objects.update_or_create(api_ID=shop['storeID'], defaults={'name': shop['storeName'], 'service_state': shop['isActive'], 'logo_url': logo})
 
         # Load offers (Game + Availability)
-        offers_url = 'https://www.cheapshark.com/api/1.0/deals'
-        offers_json = requests.get(offers_url).json()
 
-        for offer in offers_json:
-            if offer['releaseDate'] > 0:
-                correct_date = datetime.fromtimestamp(offer['releaseDate'])
-            else:
-                correct_date = datetime(2000,1,1) # If the game doesn't have a specified date
+        PAGES_TO_DOWNLOAD = 8   # Adjustable depending on how much data do we want
 
-            score_val = offer.get('metacriticScore', 0)
-            if score_val is None or score_val == "0":
-                score_val = 0   # Default value for non-existent scores
+        for page in range(PAGES_TO_DOWNLOAD):
 
-            game_obj, _ = Game.objects.update_or_create(api_ID=offer['gameID'], defaults={'title': offer['title'], 'score': float(score_val), 'launch_date': correct_date, 'cover_url': offer['thumb']})
+            offers_url = f'https://www.cheapshark.com/api/1.0/deals?pageNumber={page}'
+            offers_json = requests.get(offers_url).json()
 
-            try:
-                shop_obj = Shop.objects.get(api_ID=offer['storeID'])
-                offer_link = f"https://www.cheapshark.com/redirect?dealID={offer['dealID']}"
-                Availability.objects.update_or_create(game=game_obj, shop=shop_obj, defaults={'current_price': offer['salePrice'], 'hist_min_price': offer['salePrice'], 'offer_url': offer_link})
-            except Shop.DoesNotExist:   # Safety check
-                continue
+            for offer in offers_json:
+                if offer['releaseDate'] > 0:
+                    correct_date = datetime.fromtimestamp(offer['releaseDate'])
+                else:
+                    correct_date = datetime(2000,1,1) # If the game doesn't have a specified date
+
+                score_val = offer.get('metacriticScore', 0)
+                if score_val is None or score_val == "0":
+                    score_val = 0   # Default value for non-existent scores
+
+                game_obj, _ = Game.objects.update_or_create(api_ID=offer['gameID'], defaults={'title': offer['title'], 'score': float(score_val), 'launch_date': correct_date, 'cover_url': offer['thumb']})
+
+                try:
+                    shop_obj = Shop.objects.get(api_ID=offer['storeID'])
+                    offer_link = f"https://www.cheapshark.com/redirect?dealID={offer['dealID']}"
+                    Availability.objects.update_or_create(game=game_obj, shop=shop_obj, defaults={'current_price': offer['salePrice'], 'hist_min_price': offer['salePrice'], 'offer_url': offer_link})
+                except Shop.DoesNotExist:   # Safety check
+                    continue
 
         self.stdout.write(self.style.SUCCESS("Data loaded successfully"))
