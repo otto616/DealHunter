a35fa92eefc8e6a27c13475f7a79f39c3342f67e
otto616 <ottobielrodenas@gmail.com>
2026-02-17 20:24:13 +0100

    frontend

--- games/views.py
+++ games/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Game
 
 # Create your views here.
+
+def index(request):
+
+    games = Game.objects.all()
+
+    return render(request, 'games/index.html', {'games': games})
