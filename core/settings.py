7c6024aadea94704e2ccda05f21412bb70caae67
otto616 <ottobielrodenas@gmail.com>
2026-02-19 15:44:10 +0100

    login/logout

--- core/settings.py
+++ core/settings.py
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# --- LOGIN / LOGOUT ---
+LOGIN_REDIRECT_URL = 'games:home'
+LOGOUT_REDIRECT_URL = 'games:home'
