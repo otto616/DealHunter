5ede9a132cf644233c5c49e6dfcf23be5d02d781
otto616 <ottobielrodenas@gmail.com>
2026-02-19 14:36:26 +0100

    detail_page

--- games/urls.py
+++ games/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'games'
 urlpatterns = [
-    path('', views.home, name='index'),
+    path('', views.GameListView.as_view(), name='home'),
+    path('game/<int:pk>/', views.GameDetailView.as_view(), name='detail'),
 ]
--- games/views.py
+++ games/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Game
 
 # Create your views here.
 
-def home(request):
-
+'''def home(request):  # Home page of the web
     games = Game.objects.all()
 
-    return render(request, 'games/home.html', {'games': games})
+    return render(request, 'games/home.html', {'games': games})'''
+
+class GameListView(ListView):
+    model = Game
+    template_name = 'games/home.html'
+    context_object_name = 'games'
+
+class GameDetailView(DetailView):
+    model = Game
+    template_name = 'games/detail.html'
+    context_object_name = 'game'
